16e38611f921bb96802e13a60fa7a80a1a76fd32
pireats-abhi <pireats.abhi@gmail.com>
2020-01-12 18:36:18 +0600

    update verson 3.0.2

--- BubbleDraw/src/BubblePanel.java
+++ BubbleDraw/src/BubblePanel.java
@@ -1,18 +1,113 @@
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+import java.awt.event.MouseWheelEvent;
+import java.awt.event.MouseWheelListener;
 import java.util.ArrayList;
 
 import javax.swing.JPanel;
+import javax.swing.Timer;
 
 public class BubblePanel extends JPanel {
 
 	private ArrayList<Bubble> bubbleList;
+	private int size = 30;
+	private Timer timer;
+	private final int DELAY = 33; //ms of delay for 30fps
 
 	public BubblePanel() {
 		bubbleList = new ArrayList<Bubble>();
+		addMouseListener(new BubbleListener());
+		addMouseMotionListener(new BubbleListener());
+		addMouseWheelListener(new BubbleListener());
+		
+		timer = new Timer (DELAY, new BubbleListener());
 
 		setBackground(Color.black);
 		setPreferredSize(new Dimension(600, 400));
+		
+		timer.start();
+
+	}
+
+	public void paintComponent(Graphics page) {
+		super.paintComponent(page);
+
+		// draw all the bubbles from bbble list
+		for (Bubble bubble : bubbleList) {
+			page.setColor(bubble.color);
+			page.fillOval(bubble.x - bubble.size / 2, bubble.y - bubble.size / 2, bubble.size, bubble.size);
+		}
+
+		// write the numbers of bubbles on screen
+		page.setColor(Color.GREEN);
+		page.drawString("Count: " + bubbleList.size(), 5, 15);
+	}
+
+	private class BubbleListener implements MouseListener, MouseMotionListener, MouseWheelListener, ActionListener {
+
+		@Override
+		public void mouseClicked(MouseEvent e) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void mousePressed(MouseEvent e) {
+			bubbleList.add(new Bubble(e.getX(), e.getY(), size));
+			repaint();
+		}
+
+		@Override
+		public void mouseReleased(MouseEvent e) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void mouseEntered(MouseEvent e) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void mouseExited(MouseEvent e) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void mouseDragged(MouseEvent e) {
+			bubbleList.add(new Bubble(e.getX(), e.getY(), size));
+			repaint();
+			
+		}
+
+		@Override
+		public void mouseMoved(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mouseWheelMoved(MouseWheelEvent e) {
+			size -= e.getWheelRotation();
+			
+		}
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			for(Bubble bubble:bubbleList) {
+				bubble.update();
+			}
+			repaint();
+			
+		}
 
 	}
 
@@ -21,11 +116,17 @@ public class BubblePanel extends JPanel {
 		public int x;
 		public int y;
 		public int size;
+		public Color color;
 
 		public Bubble(int newX, int newY, int newSize) {
 			x = newX;
 			y = newY;
 			size = newSize;
+			color = new Color ((float)Math.random(), (float)Math.random(), (float)Math.random());
+		}
+		
+		public void update() {
+			y -= 5; // float each bubble up 5 pixel per frame
 		}
 	}
 
